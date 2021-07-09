c95739422ce871d7db0c5a870d27000d24cfe72a
Claudio Barsante <clbmribas@gmail.com>
2021-07-09 10:51:02 -0300

    reverse integer

---
+++ leetcode/strings/reverse-integer.js
@@ -0,0 +1,66 @@
+/**
+ * 7. Reverse Integer
+Easy
+
+5099
+
+7698
+
+Add to List
+
+Share
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+Example 4:
+
+Input: x = 0
+Output: 0
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+ */
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+  const temp = x.toString().split('');
+
+  let isNegative = false;
+
+  if (temp[0] === '-') {
+    isNegative = true;
+    temp.shift();
+  }
+
+  let reversed = temp.reverse().join('');
+
+  if (isNegative) {
+    reversed = '-' + reversed;
+  }
+
+  let result = Number(reversed);
+
+  if (result < Math.pow(-2, 31) || result > Math.pow(2, 31)) return 0;
+
+  return result;
+};
