086d7912d83338594251dbb367b7e803a35787bc
Claudio Barsante <clbmribas@gmail.com>
2021-07-09 10:31:58 -0300

    reverse-string

---
+++ leetcode/strings/reverse-string.js
@@ -0,0 +1,45 @@
+/*
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+ 
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is a printable ascii character.
+ 
+
+Follow up: Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+   Hide Hint #1  
+The entire logic for reversing a string is based on using the opposite directional two-pointer approach!
+*/
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    const temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+    left++;
+    right--;
+  }
+};
+
+console.log(reverseString(['h', 'e', 'l', 'l', 'o']));
