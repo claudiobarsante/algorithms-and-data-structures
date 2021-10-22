ca2a52d770c1d57c8630698ba1428b0633d1c372
Claudio Barsante <clbmribas@gmail.com>
2021-10-22 16:17:34 -0300

    feat: add edge cases examples to spiral matrix solution

--- leetcode/matrix/spiral-matrix.js
+++ leetcode/matrix/spiral-matrix.js
@@ -36,46 +36,67 @@ Accepted
 Submissions
 1,531,549
  */
-var spiralOrder = function(matrix) {
-    
-     let result =[];
-     
-     let rowStart = 0;
-     let rowEnd = matrix.length-1;
-     
-     let colStart = 0;
-     let colEnd = matrix[0].length-1;
-     
-     while(rowStart <=rowEnd && colStart<=colEnd){
-         // -- top
-         for(let col = colStart; col <=colEnd; col++){
-             result.push(matrix[rowStart][col]);
-         }
-         // -- right border
-         for(let row = rowStart+1; row <=rowEnd; row++){
-             result.push(matrix[row][colEnd]);
-         }
-         // -- bottom
-         for(let col= colEnd-1; col >=colStart; col--){ 
-             /**Handle the edge case when there's a single row in the middle of the matrix.
-              * We don't want to double-count the values in this row, which we've already handled.
-              * counted in the top traverse
-              *  */ 
-             if(rowStart === rowEnd) break;          
-             result.push(matrix[rowEnd][col]);
-         }
-         // -- left border
-         for(let row = rowEnd-1; row >rowStart; row--){
-          /** Handle the edge case when there's a single column in the middle of the matrix.
-           * We don't want to double-count the values in this column, which we've already handled.
-              * counted in the right border traverse
-          */
-             if(colStart === colEnd) break;
-             result.push(matrix[row][colStart]);
-         }
-         rowStart++;
-         rowEnd--;
-         colStart++;
-         colEnd--;
-         
-     }
+var spiralOrder = function (matrix) {
+  let result = [];
+
+  let rowStart = 0;
+  let rowEnd = matrix.length - 1;
+
+  let colStart = 0;
+  let colEnd = matrix[0].length - 1;
+
+  while (rowStart <= rowEnd && colStart <= colEnd) {
+    // -- top
+    for (let col = colStart; col <= colEnd; col++) {
+      result.push(matrix[rowStart][col]);
+    }
+    // -- right border
+    for (let row = rowStart + 1; row <= rowEnd; row++) {
+      result.push(matrix[row][colEnd]);
+    }
+    // -- bottom
+    for (let col = colEnd - 1; col >= colStart; col--) {
+      /**Handle the edge case when there's a single row in the middle of the matrix.
+       * We don't want to double-count the values in this row, which we've already handled.
+       * counted in the top traverse
+       * edge case 1 */
+      if (rowStart === rowEnd) break;
+      result.push(matrix[rowEnd][col]);
+    }
+    // -- left border
+    for (let row = rowEnd - 1; row > rowStart; row--) {
+      /** Handle the edge case when there's a single column in the middle of the matrix.
+       * We don't want to double-count the values in this column, which we've already handled.
+       * counted in the right border traverse
+       * edge case 2
+       */
+      if (colStart === colEnd) break;
+      result.push(matrix[row][colStart]);
+    }
+    rowStart++;
+    rowEnd--;
+    colStart++;
+    colEnd--;
+  }
+};
+/**Edge case 1
+     * {
+  "array": [
+    [1, 2, 3, 4],
+    [10, 11, 12, 5],<--|11,12|
+    [9, 8, 7, 6]
+  ]
+}*/
+
+/**Edge case 2
+ * {
+  "array": [
+    [1, 2, 3],
+    [12, 13, 4],--|13|
+    [11, 14, 5],--|14|
+    [10, 15, 6],--|15|
+    [9, 8, 7]
+  ]
+}
+ * 
+*/
