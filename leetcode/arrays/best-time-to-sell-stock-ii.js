3a464e8516eab25f96840fa018f660540c898ec6
Claudio Barsante <clbmribas@gmail.com>
2023-07-18 11:19:48 -0300

    refactor: maxProfit solution

--- leetcode/arrays/best-time-to-sell-stock-ii.js
+++ leetcode/arrays/best-time-to-sell-stock-ii.js
@@ -35,19 +35,20 @@ Constraints:
  * @return {number}
  */
 var maxProfit = function (prices) {
-  let start = prices[0];
-  let profit = 0;
+  let max = 0;
 
+  let buy = prices[0];
+  let sell;
+  let currentProfit = 0;
   for (let i = 1; i < prices.length; i++) {
-    const price = prices[i];
-
-    if (price < start) {
-      start = price;
+    sell = prices[i];
+    if (buy > sell) {
+      buy = sell;
     } else {
-      profit += price - start;
-      start = price;
+      currentProfit = sell - buy;
+      max = Math.max(max, currentProfit);
     }
   }
 
-  return profit;
+  return max;
 };
