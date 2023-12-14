04f662c5ffbd3df4063d98864ef1775d26c0271e
Claudio Barsante <clbmribas@gmail.com>
2023-12-14 16:43:55 -0300

    feat: add leetcode - 35.search insert position solution

---
+++ leetcode/arrays/search-insert-position.js
@@ -0,0 +1,56 @@
+/*35. Search Insert Position
+Solved
+Easy
+Topics
+Companies
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid; // Target found at index mid.
+    } else if (nums[mid] < target) {
+      left = mid + 1; // Adjust the search range to the right half.
+    } else {
+      right = mid - 1; // Adjust the search range to the left half.
+    }
+  }
+
+  // If the target is not found, left represents the index where the target should be inserted.
+  return left;
+};
